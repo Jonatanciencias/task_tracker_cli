b4eebe8bf83c3e2ce81447d1efc83f5dcc40caf5
Jonatan <jonatanciencias@gmail.com>
2024-09-12 08:16:21 -0500

    task manager basic structure with empty functions

---
+++ src/task_manager.py
@@ -0,0 +1,32 @@
+# src/task_manager.py
+
+import json
+from datetime import datetime
+import os
+
+
+TASK_FILE = 'tasks.json'
+
+# 1. Load tasks from file
+def load_tasks():
+    pass
+
+# 2. Save tasks to file
+def save_tasks(tasks):
+    pass    
+
+# 3. Add task to file
+def add_task(description):
+    pass
+
+def list_tasks():
+    pass
+
+def update_task(task_id, new_description):
+    pass
+
+def delete_task(task_id):
+    pass
+
+def status_task(task_id):
+    pass

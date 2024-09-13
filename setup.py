3d9928d2483d9ea7e3b01d42039f67f0a32c76ba
Jonatan <jonatanciencias@gmail.com>
2024-09-13 15:44:35 -0500

    Refactor setup.py to improve package structure and installation process

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 """This file is necessary for the package to be installed with pip."""
 from setuptools import setup, find_packages
 
+from setuptools import setup, find_packages
+
 setup(
     name="task-tracker-cli",
     version="1.0.0",
-    packages=find_packages(),
-    install_requires=[],  # Dependencies needed for this project
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],
     entry_points={
         'console_scripts': [
-            'task-cli=src.cli:main',
+            'task-cli=cli:main',
         ],
     },
 )

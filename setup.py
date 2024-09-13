2a4324369fe82254b23c39fb83b2e5ec0b2855cc
Jonatan <jonatanciencias@gmail.com>
2024-09-13 18:35:10 -0500

    Refactor setup.py to improve package structure and installation process

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+"""Library configuration file."""
 from setuptools import setup, find_packages
 
 setup(
     name='task-tracker-cli',
     version='1.0.0',
-    packages=find_packages(),
-    install_requires=[],
+    packages=find_packages(where="src"),  # Search for packages in the 'src' directory
+    package_dir={"": "src"},  # Specify that the source code is in the 'src' folder
+    install_requires=[],  # If you need to install any dependencies
     entry_points={
         'console_scripts': [
-            'task-cli=cli:main',
+            'task-cli=cli:main',  # Specify that the 'task-cli' command will execute the 'main' function in 'cli.py'
         ],
     },
 )
